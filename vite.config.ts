fab856107db754a16e5c0bbdd39aecf2b2ee8782
Ahmed mazen dessouky <amd07dev@gmail.com>
2025-01-11 10:34:39 +0100

    edit seo

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import {defineConfig} from "vite"
 import react from "@vitejs/plugin-react"
 import path from "path"
+import sitemap from "vite-plugin-sitemap"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: "https://www.amazentrust.com/",
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
